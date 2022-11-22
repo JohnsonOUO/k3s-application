39588bd6ed1c27df59259edc6c9be14ef3a0e374
chris <c090350219@gmail.com>
2022-11-22 09:58:36 +0800

    update

--- master2.sh
+++ master2.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+if [ ! -n "$1"  ]; then
+    echo "Please put ~ at the end"
+
 echo "Label the node that we can deploy the pod on a selected node"
 kubectl label node worker node-role.kubernetes.io/worker=worker
 kubectl label node worker id=worker
 kubectl label node master id=master
+
+echo "Check Resource"
+source=$(ls /etc | grep colas)
+if [ ! -n "$source" ]; then
+    mkdir /etc/colas
+    touch /etc/colas/cola1
+    touch /etc/colas/cola2
+fi
+echo "Check finish"
 
 echo "Check helm"
 check=$(ls /usr/local/bin | grep helm)
@@ -13,7 +25,9 @@ if [ ! -n "$check" ]; then
     chmod 700 get_helm.sh
     ./get_helm.sh
     rm get_helm.sh
+    echo "install helm finish"
 fi
 echo "deploy device plugin"
-cd ~/k3s-application/kv260-k3s
+cd $1/k3s-application/kv260-k3s
 make deploy
+echo "deploy finish"
--- master3.sh
+++ master3.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+if [ ! -n "$1"  ]; then
+    echo "Please put ~ at the end"
+
 echo "get Vitis-AI"
-cd ~/k3s-application
+cd $1/k3s-application
 git clone https://github.com/Xilinx/Vitis-AI.git
 
-echo "copy" 
-cp ~/k3s-application/need/* ~/k3s-application/Vitis-AI
+echo "copy necessary file" 
+cp $1/k3s-application/need/* ~/k3s-application/Vitis-AI
 
 echo "copy training file"
-cp -r ~/k3s-application/ALLFORDEMO ~/k3s-application/Vitis-AI
+cp -r $1/k3s-application/ALLFORDEMO ~/k3s-application/Vitis-AI
 
 echo "copy socket file"
-cp ~/k3s-application/vitis/vitis-server.py ~/k3s-application/Vitis-AI
+cp $1/k3s-application/vitis/vitis-server.py ~/k3s-application/Vitis-AI
 
-echo "mkdir"
+echo "mkdir Path for Vitis-AI"
 
 mkdir /dev/shm
 mkdir /opt/xilinx/overlaybins
